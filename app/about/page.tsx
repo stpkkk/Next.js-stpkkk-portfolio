32831e0a6851b0cc293374023ce170fda33187e9
Igor Stepanov <stipyk1309@gmail.com>
2023-05-20 17:20:40 +0300

    AboutMe page create

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,9 +1,48 @@
 import React from "react";
+import Image from "next/image";
+import { Skills } from "@components/Skills";
 
-type Props = {};
+const AboutPage: React.FC = () => {
+  const getParagraphs = [
+    {
+      id: 0,
+      text: "I`m a React developer from Petrozavodsk, I am passionate about creating highly dynamic and interactive user interfaces that seamlessly integrate with a variety of technologies. With years of experience in the field, I have honed my skills in both front-end development and back-end integration, allowing me to deliver complex web applications that meet the needs of my clients.",
+    },
+    {
+      id: 1,
+      text: "My expertise in React allows me to create scalable and performant applications that are easy to maintain and update over time. I am dedicated to staying up-to-date with the latest advancements in the React ecosystem, and I am constantly exploring new libraries and tools that can help me build better applications.",
+    },
+    {
+      id: 2,
+      text: "In addition to my technical skills, I am also a strong collaborator and communicator. I understand the importance of working closely with clients and other team members to ensure that the final product meets their needs and exceeds their expectations. I am always open to feedback and willing to make changes to ensure that the end result is a product that everyone is proud of.",
+    },
+    {
+      id: 3,
+      text: "Whether you are looking to build a new web application from scratch or need help updating an existing project, I am confident that I have the skills and experience to deliver results that meet your needs. So if you are looking for a talented and experienced React developer to join your team, please don't hesitate to get in touch!",
+    },
+  ];
 
-const AboutPage: React.FC = (props: Props) => {
-  return <div>AboutPage</div>;
+  return (
+    <div className="flex_between flex-wrap">
+      <div className="bg-secondary flex_center flex-col max-w-[700px] w-full self-start rounded-lg sm:rounded-none">
+        <Image
+          src={"/assets/images/about.jpg"}
+          alt="about"
+          width={600}
+          height={400}
+        />
+        <Skills />
+      </div>
+      <div className="basis-[700px] sm:p-4">
+        <h1 className="head_text mb-4 sm:text-center">About Me</h1>
+        <ul className="flex flex-col gap-y-4 font-bree">
+          {getParagraphs.map(paragraph => (
+            <li key={paragraph.id}>{paragraph.text}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default AboutPage;
---
+++ components/Skills.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import {
+  SiReact,
+  SiTypescript,
+  SiRedux,
+  SiAxios,
+  SiTailwindcss,
+  SiGit,
+  SiNextdotjs,
+  SiSass,
+  SiStyledcomponents,
+  SiJavascript,
+} from "react-icons/si";
+
+interface ISkill {
+  id: number;
+  Icon: React.FC<{ size?: number }>;
+  title: string;
+}
+
+const skillList: ISkill[] = [
+  { id: 0, Icon: SiNextdotjs, title: "Next.js" },
+  { id: 1, Icon: SiReact, title: "React" },
+  { id: 2, Icon: SiJavascript, title: "JavaScript" },
+  { id: 3, Icon: SiTypescript, title: "TypeScript" },
+  { id: 4, Icon: SiRedux, title: "Redux" },
+  { id: 5, Icon: SiAxios, title: "Axios" },
+  { id: 6, Icon: SiTailwindcss, title: "Tailwind" },
+  { id: 7, Icon: SiSass, title: "Sass" },
+  { id: 8, Icon: SiStyledcomponents, title: "Styled Components" },
+  { id: 9, Icon: SiGit, title: "Git" },
+];
+
+export const Skills: React.FC = () => {
+  return (
+    <div className="p-4">
+      <ul className="flex flex-wrap items-center justify-center mx-auto gap-[20px] after:empty:flex-1">
+        {skillList.map(({ id, Icon }) => (
+          <li key={id} className="rounded-full text-black">
+            <Icon size={40} />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
