62d3fc101bf6a13e69440bf4428d4e7b0a039c49
Igor Stepanov <stipyk1309@gmail.com>
2023-05-27 18:01:07 +0300

    ContactForm add validation

--- components/contact/ContactForm.tsx
+++ components/contact/ContactForm.tsx
@@ -1,103 +1,140 @@
 "use client";
-import React, { useRef, useState } from "react";
-import emailjs from "@emailjs/browser";
+import React, { useState, ChangeEvent, FormEvent } from "react";
+
+interface FormErrors {
+  name?: string;
+  email?: string;
+  message?: string;
+}
+
+interface FormData {
+  name: string;
+  email: string;
+  message: string;
+}
 
 export const ContactForm: React.FC = () => {
-  const formRef = useRef<HTMLFormElement | null>(null);
-  const [form, setForm] = useState({
+  const [formData, setFormData] = useState<FormData>({
     name: "",
     email: "",
     message: "",
   });
-  const [loading, setLoading] = useState(false);
+  const [errors, setErrors] = useState<FormErrors>({});
 
   const handleChange = (
-    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+    e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLTextAreaElement>
   ) => {
-    const { name, value } = e.target;
-    setForm({ ...form, [name]: value });
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
   };
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  const validateForm = () => {
+    const validationErrors: FormErrors = {};
+
+    if (!formData.name.trim()) {
+      validationErrors.name = "Name is required";
+    }
+
+    if (!formData.email.trim()) {
+      validationErrors.email = "Email is required";
+    } else if (!/\S+@\S+\.\S+/.test(formData.email)) {
+      validationErrors.email = "Invalid email address";
+    }
+
+    if (!formData.message.trim()) {
+      validationErrors.message = "Message is required";
+    }
+
+    return validationErrors;
+  };
+
+  const handleSubmit = (e: FormEvent) => {
     e.preventDefault();
-    setLoading(true);
-    emailjs
-      .send(
-        "service_0jm95v8", //from services
-        "template_4yabinl", //from templates
-        {
-          from_name: form.name,
-          to_name: "Igor",
-          from_email: form.email,
-          to_email: "stipyk1309@gmail.com",
-          message: form.message,
-        },
-        "uYcNVfB6L-ldGWARD" //public key
-      )
-      .then(
-        () => {
-          setLoading(false);
-          alert("Thank you. I will get back to you as soon as possible 🫡.");
 
-          setForm({
-            name: "",
-            email: "",
-            message: "",
-          });
-        },
-        error => {
-          setLoading(false);
-          console.log(error);
-          alert("Something went wrong 😢");
-        }
-      );
+    const validationErrors = validateForm();
+    if (Object.keys(validationErrors).length > 0) {
+      setErrors(validationErrors);
+      return;
+    }
+
+    // Submit form logic goes here
+    console.log("Form submitted:", formData);
+    setFormData({
+      name: "",
+      email: "",
+      message: "",
+    });
+    setErrors({});
   };
+
   return (
-    <form
-      ref={formRef as React.RefObject<HTMLFormElement>}
-      onSubmit={handleSubmit}
-      className="flex flex-1 flex-col gap-8 bg-[#ffffff10] text-white font-bree  rounded-lg p-8 sm:gap-4 sm:p-4"
-    >
-      <h2>GET IN TOUCH 😊</h2>
-      <label htmlFor="name" className="flex flex-col">
-        <span className="font-medium mb-3">Your Name</span>
+    <form className="max-w-md mx-auto" onSubmit={handleSubmit}>
+      <div className="mb-4">
+        <label
+          htmlFor="name"
+          className="block mb-2 font-semibold text-gray-700"
+        >
+          Name
+        </label>
         <input
-          onChange={handleChange}
           type="text"
+          id="name"
           name="name"
-          value={form.name}
-          placeholder="What`s your name?"
-          className="bg-gray-100 p-3 rounded-lg outline-none border-none font-medium text-black"
+          value={formData.name}
+          onChange={handleChange}
+          className={`w-full px-3 py-2 border ${
+            errors.name ? "border-red-500" : "border-gray-300"
+          } rounded-md`}
         />
-      </label>
-      <label htmlFor="email" className="flex flex-col">
-        <span className="font-medium mb-3">Your Email</span>
+        {errors.name && <p className="text-red-500 mt-1">{errors.name}</p>}
+      </div>
+      <div className="mb-4">
+        <label
+          htmlFor="email"
+          className="block mb-2 font-semibold text-gray-700"
+        >
+          Email
+        </label>
         <input
           type="email"
+          id="email"
           name="email"
-          value={form.email}
+          value={formData.email}
           onChange={handleChange}
-          placeholder="What`s your email?"
-          className="bg-gray-100 p-3 rounded-lg outline-none border-none font-medium text-black"
+          className={`w-full px-3 py-2 border ${
+            errors.email ? "border-red-500" : "border-gray-300"
+          } rounded-md`}
         />
-      </label>
-      <label htmlFor="message" className="flex flex-col">
-        <span className="font-medium mb-3">Your Message</span>
+        {errors.email && <p className="text-red-500 mt-1">{errors.email}</p>}
+      </div>
+      <div className="mb-4">
+        <label
+          htmlFor="message"
+          className="block mb-2 font-semibold text-gray-700"
+        >
+          Message
+        </label>
         <textarea
-          className="bg-gray-100 p-3 rounded-lg outline-none border-none font-medium text-black"
+          id="message"
           name="message"
-          value={form.message}
-          placeholder="What do you want to say?"
+          rows={4}
+          value={formData.message}
           onChange={handleChange}
-          cols={30}
-          rows={7}
-        />
-      </label>
+          className={`w-full px-3 py-2 border ${
+            errors.message ? "border-red-500" : "border-gray-300"
+          } rounded-md`}
+        ></textarea>
+        {errors.message && (
+          <p className="text-red-500 mt-1">{errors.message}</p>
+        )}
+      </div>
       <button
         type="submit"
-        className="bg-gray-100 py-3 px-8 outline-none w-fit rounded-lg font-bold shadow-md text-black"
+        className="px-4 py-2 bg-blue-500 text-white rounded-md"
       >
-        {loading ? "Sending..." : "Send"}
+        Submit
       </button>
     </form>
   );
