21596075248a8872b91255ed705c8186eae19394
Igor Stepanov <stipyk1309@gmail.com>
2023-06-29 09:49:47 +0300

    experience data add

---
+++ data/experience.ts
@@ -0,0 +1,68 @@
+import { IExperience } from "@models/experience";
+import { MdWorkOutline } from "react-icons/md";
+
+export const experience: IExperience[] = [
+  {
+    id: 0,
+    title: "Rukki Pro",
+    dateStart: "2023-02-01T00:00:00.035Z",
+    dateEnd: "2023-06-15T00:00:00.035Z",
+    location: "Moscow",
+    link: "https://rukki.pro/",
+    linkTitle: "rukki.pro",
+    position: "Junior Frontend-Developer",
+    description:
+      "Worked remotely, participated in the implementation of interfaces. I joined the project at the final stage and successfully cooperated with the team. I successfully eliminated all interface bugs, optimized the application, at the moment we are finishing the implementation of all business requirements and, together with the team, we preparing the project for production",
+    stack: [
+      "Next.js",
+      "React",
+      "Redux Toolkit",
+      "Javascript",
+      "Typescript",
+      "Styled Components",
+    ],
+    icon: MdWorkOutline,
+  },
+  {
+    id: 1,
+    title: "Technomedia",
+    dateStart: "2022-10-01T00:00:00.035Z",
+    dateEnd: "2023-02-15T00:00:00.035Z",
+    location: "Petrozavodsk",
+    link: "https://sysdyn.ru/",
+    linkTitle: "sysdyn.ru",
+    position: "Intern Frontend-Developer",
+    description:
+      "Joined the company as a novice developer, participated in discussions, fixed bugs, improved existing web applications and participated in the development of new ones",
+    stack: [
+      "React",
+      "Redux Toolkit",
+      "Javascript",
+      "Typescript",
+      "Styled Components",
+      "Tailwind",
+    ],
+    icon: MdWorkOutline,
+  },
+  {
+    id: 2,
+    title: "Freelance",
+    dateStart: "2021-12-01T00:00:00.035Z",
+    dateEnd: "2022-09-15T00:00:00.035Z",
+    location: "",
+    link: "",
+    linkTitle: "",
+    position: "Frontend-developer",
+    description:
+      "Development of web applications, creating Layouts, website and application support",
+    stack: [
+      "React",
+      "Javascript",
+      "Typescript",
+      "Styled Components",
+      "SASS",
+      "Bootstrap",
+    ],
+    icon: MdWorkOutline,
+  },
+];
---
+++ models/experience.ts
@@ -0,0 +1,15 @@
+import { IconType } from "react-icons/lib";
+
+export interface IExperience {
+  id: number;
+  title: string;
+  dateStart: string;
+  dateEnd: string;
+  location: string;
+  link: string;
+  linkTitle: string;
+  position: string;
+  description: string;
+  stack: string[];
+  icon: IconType;
+}
