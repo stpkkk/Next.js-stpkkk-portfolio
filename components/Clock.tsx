63208528ee9413949a723e8c933189367a90f505
Igor Stepanov <stipyk1309@gmail.com>
2023-10-02 13:57:59 +0300

    Clock fix error

--- components/Clock.tsx
+++ components/Clock.tsx
@@ -1,34 +1,27 @@
-"use client";
-import React, { useState, useEffect } from "react";
+'use client';
+import React, { useState, useEffect } from 'react';
 
 interface ClockProps {
   size?: number;
 }
 
 const Clock: React.FC<ClockProps> = ({ size = 100 }) => {
-  const [time, setTime] = useState(new Date());
+  const [date, setDate] = useState(new Date());
+  const [hydrated, setHydrated] = React.useState(false);
 
   useEffect(() => {
-    const timerID = setInterval(() => tick(), 1000);
-
-    return () => {
-      clearInterval(timerID);
+    const timer = setInterval(() => setDate(new Date()), 1000);
+    return function cleanup() {
+      clearInterval(timer);
     };
+  }, [date]);
+
+  useEffect(() => {
+    setHydrated(true);
   }, []);
-
-  const tick = () => {
-    setTime(new Date());
-  };
-
-  const formatDigits = (num: number): string => {
-    return num < 10 ? `0${num}` : `${num}`;
-  };
-
-  const currentDate = new Date();
-  const formattedDate = currentDate.toDateString();
-
-  const hours = formatDigits(time.getHours());
-  const minutes = formatDigits(time.getMinutes());
+  if (!hydrated) {
+    return null;
+  }
 
   const viewBoxSize = size + 20;
   const center = viewBoxSize / 2;
@@ -38,30 +31,30 @@ const Clock: React.FC<ClockProps> = ({ size = 100 }) => {
       width={100}
       height={50}
       viewBox={`0 0 ${viewBoxSize} ${viewBoxSize + 20}`}
-      xmlns="http://www.w3.org/2000/svg"
-      className="bg-black sm:w-[70px] sm:h-[40px]"
+      xmlns='http://www.w3.org/2000/svg'
+      className='bg-black sm:w-[70px] sm:h-[40px]'
     >
       <text
         x={center}
         y={center}
-        textAnchor="middle"
-        fontSize="50"
-        fontWeight="bold"
-        fill="#fff"
-        className="font-bree tracking-wide w-full sm:text-[40px]"
+        textAnchor='middle'
+        fontSize='50'
+        fontWeight='bold'
+        fill='#fff'
+        className='font-ubuntu tracking-wide w-full sm:text-[40px]'
       >
-        {`${hours}:${minutes}`}
+        {date.toLocaleTimeString()}
       </text>
       <text
         x={center}
         y={viewBoxSize}
-        textAnchor="middle"
-        fontSize="32"
-        fontWeight="bold"
-        fill="#fff"
-        className="font-bree tracking-wide w-full sm:text-[28px]"
+        textAnchor='middle'
+        fontSize='32'
+        fontWeight='bold'
+        fill='#fff'
+        className='font-ubuntu tracking-wide w-full sm:text-[28px]'
       >
-        {formattedDate}
+        {date.toLocaleDateString()}
       </text>
     </svg>
   );
