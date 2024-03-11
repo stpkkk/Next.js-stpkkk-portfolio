79ba50a813056c3832c2897b8396742627e057e6
Igor Stepanov <stipyk1309@gmail.com>
2024-03-11 19:46:15 +0300

    update: app theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,40 +1,40 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: "class",
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        amatic: ["Amatic", "sans-serif"],
-        ubuntu: ["Ubuntu", "sans-serif"],
-        roboto: ["Roboto", "sans-serif"],
-        dmDisplay: ["DM Serif Display", "sans-serif"],
-        dancingScript: ["Dancing Script", "sans-serif"],
-        bree: ["Bree Serif", "sans-serif"],
-      },
-      colors: {
-        primary: "#e0e0e0",
-        secondary: "#e4c341",
-        black: "#141414",
-        bgContainer: "#091f36",
-        bgMobileNavMenu: "#1d1e22",
-      },
-      screens: {
-        sm: { max: "768px" },
-        md: { max: "1250px" },
-      },
+	darkMode: 'class',
+	content: [
+		'./pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		extend: {
+			fontFamily: {
+				amatic: ['Amatic', 'sans-serif'],
+				ubuntu: ['Ubuntu', 'sans-serif'],
+				roboto: ['Roboto', 'sans-serif'],
+				dmDisplay: ['DM Serif Display', 'sans-serif'],
+				dancingScript: ['Dancing Script', 'sans-serif'],
+				bree: ['Bree Serif', 'sans-serif'],
+			},
+			colors: {
+				primary: '#F5F0F0',
+				secondary: '#e4c341',
+				black: '#141414',
+				bgContainer: '#1e1e1e',
+				bgMobileNavMenu: '#1d1e22',
+			},
+			screens: {
+				sm: { max: '768px' },
+				md: { max: '1250px' },
+			},
 
-      keyframes: {
-        wiggle: {
-          "0%, 100%": { transform: "rotate(-5deg)" },
-          "50%": { transform: "rotate(5deg)" },
-        },
-      },
-    },
-  },
-  plugins: [],
-};
+			keyframes: {
+				wiggle: {
+					'0%, 100%': { transform: 'rotate(-5deg)' },
+					'50%': { transform: 'rotate(5deg)' },
+				},
+			},
+		},
+	},
+	plugins: [],
+}
